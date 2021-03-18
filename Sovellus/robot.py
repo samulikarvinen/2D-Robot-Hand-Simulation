16cf76d5e252e687515f535d0e6b5d7e9148516d
Samuli Karvinen <samuli.karvinen@aalto.fi>
2021-03-18 17:46:44 +0200

    Added some functionality for the manual sliders and auto button. They still need to be connected for the robot class and update the change in graphics

---
+++ Sovellus/robot.py
@@ -0,0 +1,14 @@
+# TODO: Here the robot should have the information about its dimentions
+#       as well as angles, coordinates and functions:
+#       forward kinematics, inverse kinematics
+
+class Robot():
+    def __init__(self):
+        pass
+
+    def forward_kinematics(self):
+        pass
+
+    def inverse_kinematics(self):
+        pass
+
--- Sovellus/window.py
+++ Sovellus/window.py
@@ -8,19 +8,21 @@ class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        # setting up the window
         self.setWindowTitle("Robot hand")
-
         self.setGeometry(100, 100, 500, 400)
-        self.set_graphics()
+
+        # setting up the objects
+
+        # setting up the docks
         self.set_manualdock()
         self.set_autodock()
         self.set_suctiondock()
 
-    # setting up the graphics for the robot and item
-    def set_graphics(self):
-        self.setCentralWidget(QTextEdit())
+        # setting up the graphics
+        self.set_graphics()
 
-    # setting up the manual dock
+    '''Setting up the manual dock with its buttons and their functionalities'''
     def set_manualdock(self):
         manualDock = QDockWidget()
         manualDock.setWindowTitle("Manual Control")
@@ -36,27 +38,45 @@ class Window(QMainWindow):
         grid = QGridLayout()
 
         # adding first label and slider for the layout
-        self.label_first_slider = QLabel("1. joint \u03B8\u2081: {:.2f}\u00B0".format(0))
-        grid.addWidget(self.label_first_slider, 0, 0)
-        first_slider = QSlider(Qt.Horizontal)
-        # todo: add functionality for the slider
-        first_slider.setRange(0, 360)
+        label_first_slider = QLabel("1. joint \u03B8\u2081:")
+        grid.addWidget(label_first_slider, 0, 0)
+        self.label_first_theta = QLabel("{:.2f}\u00B0".format(0))
+        grid.addWidget(self.label_first_theta, 0, 1)
 
-        grid.addWidget(first_slider, 1, 0)
+        self.first_slider = QSlider(Qt.Horizontal)
+        self.first_slider.setRange(0, 36000)  # 360 000 in order to have 0.01 increments from 0 to 360 degrees
+        grid.addWidget(self.first_slider, 1, 0, 1, 2)
+        self.first_slider.valueChanged.connect(self.first_slider_value_changed)
 
         # adding second label and slider for the layout
-        self.label_second_slider = QLabel("2. joint \u03B8\u2082: {:.2f}\u00B0".format(0))
-        grid.addWidget(self.label_second_slider, 3, 0)
-        second_slider = QSlider(Qt.Horizontal)
-        # todo: add functionality for the slider
-        second_slider.setRange(0, 360)
+        label_second_slider = QLabel("2. joint \u03B8\u2082:")
+        grid.addWidget(label_second_slider, 2, 0)
+        self.label_second_theta = QLabel("{:.2f}\u00B0".format(0))
+        grid.addWidget(self.label_second_theta, 2, 1)
 
-        grid.addWidget(second_slider, 4, 0)
+        self.second_slider = QSlider(Qt.Horizontal)
+        self.second_slider.setRange(0, 36000)  # 360 000 in order to have 0.01 increments from 0 to 360 degrees
+        grid.addWidget(self.second_slider, 3, 0, 1, 2)
+        self.second_slider.valueChanged.connect(self.second_slider_value_changed)
 
         # insert layout into the manual widget
         manualWidget.setLayout(grid)
 
-    # setting up the auto dock
+    def first_slider_value_changed(self):
+        theta_one = self.first_slider.value()*0.01  # from increments to degrees
+        self.label_first_theta.setText("{:.2f}\u00B0".format(theta_one))
+        # todo: save the theta 1 to robot class and update the coordinates of the joints
+        #       of the robot using the robot.forward_kinematics function. After this,
+        #       update the graphical object of the robot using the information from the robot class.
+
+    def second_slider_value_changed(self):
+        theta_two = self.second_slider.value()*0.01  # from increments to degrees
+        self.label_second_theta.setText("{:.2f}\u00B0".format(theta_two))
+        # todo: save the theta 2 to robot class and update the coordinates of the joints
+        #       of the robot using the robot.forward_kinematics function. After this,
+        #       update the graphical object of the robot using the information from the robot class.
+
+    '''Setting up the auto dock its buttons and their functionalities'''
     def set_autodock(self):
         autoDock = QDockWidget()
         autoDock.setWindowTitle("Auto Control")
@@ -71,24 +91,47 @@ class Window(QMainWindow):
         # build layout
         grid = QGridLayout()
 
+        # adding the instructions label
+        label_instruction = QLabel("Give the destination\ncoordinates in pixels.")
+        label_instruction.setAlignment(Qt.AlignCenter | Qt.AlignHCenter)
+        grid.addWidget(label_instruction, 0, 0, 1, 2)
+
         # adding the x coordinate sections
         label_x_line = QLabel("x:")
-        grid.addWidget(label_x_line, 0, 0)
-        x_line = QLineEdit()
-        x_line.setValidator(QIntValidator())
-        # todo: add functionality for the x_line
-        grid.addWidget(x_line, 0, 1)
+        grid.addWidget(label_x_line, 1, 0)
+        self.x_line = QLineEdit('0')
+        self.x_line.setValidator(QIntValidator())
+        grid.addWidget(self.x_line, 1, 1)
 
         # adding the y coordinate sections
         label_y_line = QLabel("y:")
-        grid.addWidget(label_y_line, 1, 0)
-        y_line = QLineEdit()
-        y_line.setValidator(QIntValidator())
-        # todo: add functionality for the y_line
-        grid.addWidget(y_line, 1, 1)
+        grid.addWidget(label_y_line, 2, 0)
+        self.y_line = QLineEdit('0')
+        self.y_line.setValidator(QIntValidator())
+        grid.addWidget(self.y_line, 2, 1)
+
+        '''adding the move button section'''
+        # label
+        self.label_reach = QLabel("Out of reach!")
+        self.label_reach.setAlignment(Qt.AlignCenter | Qt.AlignHCenter)
+        self.label_reach.hide()
+        grid.addWidget(self.label_reach, 3, 0, 1, 2)
+
+        # button
+        move_button = QPushButton("Move")
+        grid.addWidget(move_button, 4, 0, 1, 2)
+        move_button.pressed.connect(self.move_button_pressed)
 
         # insert layout into the auto widget
         autoWidget.setLayout(grid)
+
+    def move_button_pressed(self):
+        # checking if the lines have numbers in them
+        if self.x_line.text() and self.y_line.text():
+            if 0 <= int(self.x_line.text()) <= 5 and 0 <= int(self.y_line.text()) <= 5:
+                self.label_reach.hide()
+            else:
+                self.label_reach.show()
 
     # setting up the suction dock
     def set_suctiondock(self):
@@ -98,9 +141,13 @@ class Window(QMainWindow):
         suctionDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
         self.addDockWidget(Qt.LeftDockWidgetArea, suctionDock)
 
-        # adding Widget
-        self.suctionButton = QPushButton("Suck")
-        suctionDock.setWidget(self.suctionButton)
+        # adding Widget with button
+        self.suction_button = QPushButton("Suck")
+        suctionDock.setWidget(self.suction_button)
+
+    # setting up the graphics for the robot and item
+    def set_graphics(self):
+        self.setCentralWidget(QTextEdit())
 
 
 if __name__ == '__main__':
