cda7f00436135553aa9358ad4b48fcc98588da18
Samuli Karvinen <samuli.karvinen@aalto.fi>
2021-04-26 15:02:55 +0300

    Removed unused libraries

--- Sovellus/robot.py
+++ Sovellus/robot.py
@@ -1,6 +1,5 @@
 import math
 import numpy as np
-import decimal
 from time import sleep
 
 
--- Sovellus/text_graphics_item.py
+++ Sovellus/text_graphics_item.py
@@ -1,7 +1,5 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-import numpy as np
 
 
 class TextGraphicsItem(QGraphicsItemGroup):
